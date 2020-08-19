fdbfb717caaabaa53ec7aca7669ce4c962daaa6d
Quattro <tylercook142@gmail.com>
2020-08-19 23:18:45 +0700

    Add project to Git

---
+++ Assets/MyScripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace MyGame
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] private CameraMovement cameraMovement;
+        [SerializeField] private PlayerController playerController;
+        [SerializeField] private EnemiesManager enemiesManager;
+        [SerializeField] private SoundController soundController;
+        [SerializeField] private UIManager uIManager;
+
+        private float deleyRestart = 3f;
+
+        private void Start()
+        {
+            uIManager.Init(playerController);
+            soundController.Init();
+            cameraMovement.Init(playerController.Transform);
+            playerController.Init();
+            enemiesManager.Init(playerController);
+        }
+
+        private void Update()
+        {
+            cameraMovement.RefreshPosition();
+            playerController.Refresh();
+            enemiesManager.Refresh();
+            uIManager.Refresh();
+            RestartLevel();
+        }
+
+        private void RestartLevel()
+        {
+            if (!playerController.IsAlive)
+            {
+                StartCoroutine(WaitRestart());
+            }
+        }
+
+        private IEnumerator WaitRestart()
+        {
+            yield return new WaitForSeconds(deleyRestart);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assets/MyScripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace MyGame
+{
+    [RequireComponent(typeof(MyGame.PlayerMovement))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private PlayerMovement playerMovement;
+        [SerializeField] private PlayerHealth playerHealth;
+        [SerializeField] private PlayerShooting playerShooting;
+
+        public bool Damage
+        {
+            get
+            {
+                return playerHealth.Damage;
+            }
+        }
+        public Transform Transform 
+        {
+            get
+            {
+                return gameObject.transform;
+            }
+        }
+
+        public bool IsAlive
+        {
+            get
+            {
+                return playerHealth.IsAlive;
+            }
+        }
+
+        public int CurrentHealth
+        {
+            get
+            {
+                return playerHealth.CurrentHealth;
+            }
+        }
+
+        public void Init()
+        {
+            playerMovement.Init(playerHealth);
+            playerHealth.Init(playerMovement, playerShooting);
+            playerShooting.Init(playerHealth);
+        }
+
+        public void Refresh()
+        {
+            playerMovement.RefreshMovement();
+            playerHealth.RefreshHealth();
+            playerShooting.RefreshShooting();
+        }
+
+        public void TakeDamage(int amount)
+        {
+            playerHealth.TakeDamage(amount);
+        }
+    }
+}
